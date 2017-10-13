a8e5f5b9761ea068bccefac8c0e0d0a878d450f5
binio <tbiniecki@gmail.com>
2017-10-13 17:30:01 +0100

    add service Dao test

---
+++ src/test/java/dao/ServiceDaoTest.java
@@ -0,0 +1,27 @@
+package dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ServiceDaoTest {
+    @Test
+    public void getAllServices() throws Exception {
+    }
+
+    @Test
+    public void getServicesByName() throws Exception {
+    }
+
+    @Test
+    public void getServiceByNameSorted() throws Exception {
+    }
+
+    @Test
+    public void registerService() throws Exception {
+    }
+
+}
