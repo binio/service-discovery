d6a153b4ad9d737ec1cd5dbe8d1376c4088feee6
tomasz.biniecki <tomasz.biniecki@wowcher.co.uk>
2017-10-17 15:37:29 +0100

    clean up

--- src/test/java/service/ServiceDiscoverySdkTest.java
+++ src/test/java/service/ServiceDiscoverySdkTest.java
@@ -122,8 +122,7 @@ public class ServiceDiscoverySdkTest {
 
         List<String> keys = new ArrayList<>();
         while (scan.hasNext()) {
-            String next = scan.next();
-            keys.add(next);
+            keys.add(scan.next());
         }
 
         assertEquals(10,keys.size());
