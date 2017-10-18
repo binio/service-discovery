ce43c714cb1e33629c5c99b11cbdc795fba3ce43
tomasz.biniecki <tomasz.biniecki@wowcher.co.uk>
2017-10-18 12:48:57 +0100

    refactor sort method to use stream

--- src/main/java/util/ServiceUtils.java
+++ src/main/java/util/ServiceUtils.java
@@ -3,13 +3,19 @@ package util;
 import domain.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ServiceUtils {
 
     public static List<Service> sortServices(List<Service> services) {
-        services.sort(Comparator.comparing(Service::getVersion));
-        Collections.reverse(services);
-        return services;
+        Comparator<Service> byServiceVersion = Comparator.comparing(Service::getVersion);
+        Comparator<Service> byLoadFactor = Comparator.comparing(Service::getLoadFactor);
+
+        List<Service> list = services.stream()
+                .sorted(byServiceVersion.thenComparing(byLoadFactor))
+                .collect(Collectors.toList());
+        Collections.reverse(list);
+        return list;
     }
 }
 
